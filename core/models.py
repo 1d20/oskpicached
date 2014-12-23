d26a19e5648ff84289f80999ea91fabecad94f00
Andrew Dacenko <a.s.dvornik@gmail.com>
2014-12-24 00:47:36 +0200

    Simple design

--- core/models.py
+++ core/models.py
@@ -1,8 +1,12 @@
 from django.db import models
-
+from random import randint
 
 class FileStorage(models.Model):
     f = models.FileField(upload_to="files")
 
     def __unicode__(self):
         return str(self.f)
+
+    def icon(self):
+    	# print random.randint(0,129)
+    	return randint(0,129)
